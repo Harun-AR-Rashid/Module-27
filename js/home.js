ee48bcd5817389e50a973e928175a18de1b016cf
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-20 08:07:42 +0600

    Module-27.5 done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-console.log('Home added');
+/*
+step1:add event listener
+ prevent page reload after form submit
+step2:get money to be added  to the account balance
+
+*/
+
+
+document.getElementById('add-event-listener').addEventListener('click', function(event){
+    event.preventDefault();
+    const addInputMoney=document.getElementById('input-add-money').value;
+    console.log(addInputMoney);
+    const addPinNumber=document.getElementById('add-pin-number').value;
+    console.log(addPinNumber);
+})
